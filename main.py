7d4b2a2a344ebdd9b1ccbe13e70e1994a1640f70
Алексей Горемыкин <goremykin.jr@outlook.com>
2023-04-07 19:38:12 +0500

    путь к статик

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 from uvicorn import Server, Config
 
 app = FastAPI()
 app.include_router(router)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 server = Server(Config(app, host="127.0.0.1", port=8000))
 server.run()
