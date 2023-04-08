3a1877be2e3dd7a12e3e6a770580f691521ef06b
Алексей Горемыкин <goremykin.jr@outlook.com>
2023-04-08 23:53:48 +0500

    исправил баг с часовым поясом

--- app/db/models.py
+++ app/db/models.py
@@ -1,13 +1,16 @@
 from datetime import datetime
+import pytz
 from bson import ObjectId
 from bson.binary import Binary
 from fastapi import UploadFile
+
+moscow_tz = pytz.timezone('Europe/Moscow')
 
 class UserFile():
     def __init__(self, file: UploadFile):
         self._id = ObjectId()
         self.title = file.filename
-        self.upload_date = datetime.now()
+        self.upload_date = datetime.now(moscow_tz)
         self.type = file.content_type
         self.content = Binary(file.file.read())
         ###self.user_id = ObjectId()
