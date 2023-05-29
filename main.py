7399884508ee5f3931eed8688dcb969dba78c993
Алексей Горемыкин <goremykin.jr@outlook.com>
2023-05-29 12:38:28 +0500

    uvicorn main:app --host 127.0.0.1 --port 8000

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from uvicorn import Server, Config
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-server = Server(Config(app, host="127.0.0.1", port=8000))
-server.run()
+#server = Server(Config(app, host="127.0.0.1", port=8000))
+#server.run()
